a4e2fb5ef66e99d2fd3d562df4d58731452d50ca
wyatt Orman <wyattorman@wyatts-Air.lan>
2021-08-02 11:58:59 -0400

    added index.html and script.js to repository

---
+++ script.js
@@ -0,0 +1,47 @@
+var myArray = [
+    "austin",
+    "boston",
+    "philly"
+  ];
+  
+  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+  document.body.innerHTML = randomItem;
+    console.log(myArray)
+
+
+  var myRest = [
+    "little ceasers",
+    "ruth chris",
+    "mcdonalds"
+  ];
+  
+  var randomItem = myRest[Math.floor(Math.random()*myRest.length)];
+
+  document.body.innerHTML = randomItem;
+console.log(myRest)
+  
+var myVic = [
+    "scooter",
+    "skateboard",
+    "limo"
+  ];
+  
+  var randomItem = myVic[Math.floor(Math.random()*myVic.length)];
+
+  document.body.innerHTML = randomItem;
+console.log(myVic) 
+
+
+var myEnt = [
+    "movies",
+    "musuem",
+    "city tour"
+  ];
+  
+  var randomItem = myEnt[Math.floor(Math.random()*myEnt.length)];
+
+
+  document.body.innerHTML = randomItem;
+console.log(myEnt) 
+
